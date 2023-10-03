512a05d7941605663d95d13063d532dd71ab7352
genarold <gearodour2104@gmail.com>
2023-10-02 22:04:25 -0600

    adding movement func

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module main
 
 go 1.21.1
 
+require github.com/nsf/termbox-go v1.1.1
 
 require (
-	github.com/nsf/termbox-go v1.1.1
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"os"
@@ -9,7 +10,172 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/nsf/termbox-go"
+)
+
+func reverse(arr []uuid.UUID) []uuid.UUID {
+	// Crear un nuevo arreglo para almacenar el resultado invertido
+	invertido := make([]uuid.UUID, len(arr))
+
+	// Invertir el arreglo
+	for i, j := 0, len(arr)-1; i < len(arr); i, j = i+1, j-1 {
+		invertido[i] = arr[j]
+	}
+
+	return invertido
+}
+
+type entity struct {
+	x, y     int
+	memory   []uuid.UUID
+	maze     *Maze
+	acc      int
+	lastMove Direction
+}
+
+func (e *entity) Memoize() {
+	uuidString := e.maze.grid[e.x][e.y].uuid.String()
+
+	if len(e.memory) > 0 && e.memory[len(e.memory)-1].String() != uuidString {
+		e.memory = append(e.memory, e.maze.grid[e.x][e.y].uuid)
+	} else if len(e.memory) == 0 {
+		e.memory = append(e.memory, e.maze.grid[e.x][e.y].uuid)
+	}
+
+}
+
+func (e *entity) Render() {
+	e.maze.Render(e)
+	message := "acc:" + fmt.Sprint(e.acc)
+	for i, char := range message {
+		termbox.SetCell(i, e.maze.height, char, termbox.ColorBlack, termbox.ColorLightMagenta)
+	}
+	termbox.Flush()
+	e.RenderMemory()
+}
+
+func (e *entity) RenderMemory() {
+	historyLast := []uuid.UUID{}
+
+	if len(e.memory) >= 10 {
+		historyLast = e.memory[len(e.memory)-10:]
+	} else {
+		historyLast = e.memory
+	}
+
+	// Invertir el arreglo
+	historyReverse := reverse(historyLast)
+
+	for i, v := range historyReverse {
+		uuidString := v.String()
+		for j, ch := range ">" + uuidString {
+			termbox.SetCell(j, e.maze.height+i+1, ch, termbox.ColorDefault, termbox.ColorDefault)
+		}
+		termbox.Flush()
+	}
+}
+
+func (e *entity) moveLeft() {
+	e.Memoize()
+	e.lastMove = Right
+	if e.isWin() {
+		return
+	}
+
+	if e.y < len(e.maze.grid[0])-1 && e.maze.grid[e.x][e.y-1].val != solid {
+		e.y--
+	}
+}
+
+func (e *entity) moveRight() {
+	e.Memoize()
+	e.lastMove = Left
+	if e.isWin() {
+		return
+	}
+
+	if e.y < len(e.maze.grid[0])-1 && e.maze.grid[e.x][e.y+1].val != solid {
+		e.y++
+	}
+}
+
+func (e *entity) moveUp() {
+	e.Memoize()
+	e.lastMove = Down
+	if e.isWin() {
+		return
+	}
+
+	if e.x < len(e.maze.grid)-1 && e.maze.grid[e.x-1][e.y].val != solid {
+		e.x--
+	}
+}
+
+func (e *entity) moveDown() {
+	e.Memoize()
+	e.lastMove = Up
+	if e.isWin() {
+		return
+	}
+
+	if e.x < len(e.maze.grid)-1 && e.maze.grid[e.x+1][e.y].val != solid {
+		e.x++
+	}
+}
+
+func (e *entity) isWin() bool {
+	win := e.maze.grid[e.x][e.y].val == exit
+
+	if win {
+		message := "You win!"
+		for i, char := range message {
+			termbox.SetCell(i, e.maze.height, char, termbox.ColorBlack, termbox.ColorLightMagenta)
+		}
+		termbox.Flush()
+	}
+
+	return win
+}
+
+func (e *entity) checkWall(maze *Maze) []Direction {
+	list := []Direction{}
+
+	if maze.grid[e.x][e.y+1].val != solid {
+		list = append(list, Down)
+	}
+	if maze.grid[e.x][e.y-1].val != solid {
+		list = append(list, Up)
+	}
+	if maze.grid[e.x+1][e.y].val != solid {
+		list = append(list, Right)
+	}
+	if maze.grid[e.x-1][e.y].val != solid {
+		list = append(list, Left)
+	}
+
+	found := false
+
+	for _, dir := range list {
+		if e.lastMove == dir {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		return []Direction{e.lastMove}
+	}
+
+	return list
+}
+
+type block int
+
+const (
+	empty block = iota
+	solid
+	exit
 )
 
 type Direction int
@@ -21,21 +187,55 @@ const (
 	Right
 )
 
-type Maze struct {
-	width, height int
-	grid          [][]int
+type node int
+
+const (
+	regular node = iota
+	wall
+	branch
+	deadPoint
+	merge
+)
+
+type Cell struct {
+	uuid     uuid.UUID
+	val      block
+	nodeType node
 }
 
-var defaultMaze = [][]int{
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 0, 0, 0, 0, 1},
-	{1, 0, 1, 1, 1, 1, 1, 0, 1},
-	{1, 0, 1, 0, 0, 0, 1, 0, 1},
-	{1, 0, 1, 0, 1, 0, 1, 0, 1},
-	{1, 0, 1, 0, 1, 3, 1, 0, 1},
-	{1, 0, 1, 0, 1, 1, 1, 0, 1},
-	{1, 0, 0, 0, 0, 0, 1, 0, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1},
+func genCells(maze [][]block) [][]Cell {
+	cells := make([][]Cell, len(maze))
+
+	for i, row := range maze {
+		cells[i] = make([]Cell, len(row))
+
+		for j, val := range row {
+			cells[i][j] = Cell{
+				uuid:     uuid.New(),
+				val:      block(val),
+				nodeType: regular,
+			}
+		}
+	}
+
+	return cells
+}
+
+type Maze struct {
+	width, height int
+	grid          [][]Cell
+}
+
+var defaultMaze = [][]block{
+	{solid, solid, solid, solid, solid, solid, solid, solid, solid},
+	{solid, empty, empty, empty, empty, empty, empty, empty, solid},
+	{solid, empty, solid, solid, solid, solid, solid, empty, solid},
+	{solid, empty, solid, empty, empty, empty, solid, empty, solid},
+	{solid, empty, solid, empty, solid, empty, solid, empty, solid},
+	{solid, empty, solid, empty, solid, exit, solid, empty, solid},
+	{solid, empty, solid, empty, solid, solid, solid, empty, solid},
+	{solid, empty, empty, empty, empty, empty, solid, empty, solid},
+	{solid, solid, solid, solid, solid, solid, solid, solid, solid},
 }
 
 // NewMaze crea un nuevo objeto Maze con el tamaño especificado.
@@ -43,117 +243,54 @@ func NewMaze() (*Maze, error) {
 	maze := &Maze{
 		width:  len(defaultMaze[0]),
 		height: len(defaultMaze),
-		grid:   defaultMaze,
+		grid:   genCells(defaultMaze),
 	}
 
 	return maze, nil
 }
 
 // Render dibuja el laberinto en la terminal.
-func (m *Maze) Render(pos *map[string]int) {
+func (m *Maze) Render(e *entity) {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
 	for i, row := range m.grid {
 
-		for j, value := range row {
+		for j, cell := range row {
 			char := ' '
-			if value == 0 {
+			fg := termbox.ColorDefault
+			bg := termbox.ColorLightGray
+			if cell.val == empty {
 				char = ' '
+				fg = termbox.ColorDefault
+				bg = termbox.ColorDefault
 			}
 
-			if value == 1 {
+			if cell.val == solid {
 				char = 'W'
+				fg = termbox.ColorBlack
+				bg = termbox.ColorYellow
 			}
 
-			if value == 3 {
+			if cell.val == exit {
 				char = 'S'
+				fg = termbox.ColorRed
+				bg = termbox.ColorLightGreen
 			}
 
-			if (*pos)["x"] == i && (*pos)["y"] == j {
+			if e.x == i && e.y == j {
 				char = 'T'
+				fg = termbox.ColorGreen
+				bg = termbox.ColorDefault
 			}
 
-			termbox.SetCell(j, i, char, termbox.ColorDefault, termbox.ColorDefault)
+			termbox.SetCell(j, i, char, fg, bg)
 		}
 	}
 
 	termbox.Flush()
 }
 
-func moveLeft(maze *Maze, pos *map[string]int) {
-	if isWin(maze, pos) {
-		return
-	}
-
-	if (*pos)["y"] < len(maze.grid[0])-1 && maze.grid[(*pos)["x"]][(*pos)["y"]-1] != 1 {
-		(*pos)["y"]--
-	}
-}
-
-func moveRight(maze *Maze, pos *map[string]int) {
-	if isWin(maze, pos) {
-		return
-	}
-
-	if (*pos)["y"] < len(maze.grid[0])-1 && maze.grid[(*pos)["x"]][(*pos)["y"]+1] != 1 {
-		(*pos)["y"]++
-	}
-}
-
-func moveUp(maze *Maze, pos *map[string]int) {
-	if isWin(maze, pos) {
-		return
-	}
-
-	if (*pos)["x"] < len(maze.grid)-1 && maze.grid[(*pos)["x"]-1][(*pos)["y"]] != 1 {
-		(*pos)["x"]--
-	}
-}
-
-func moveDown(maze *Maze, pos *map[string]int) {
-	if isWin(maze, pos) {
-		return
-	}
-
-	if (*pos)["x"] < len(maze.grid)-1 && maze.grid[(*pos)["x"]+1][(*pos)["y"]] != 1 {
-		(*pos)["x"]++
-	}
-}
-
-func isWin(maze *Maze, pos *map[string]int) bool {
-	message := "You win!"
-
-	if maze.grid[(*pos)["x"]][(*pos)["y"]] == 3 {
-		for _, char := range message {
-			// fmt.Println(string(char))
-			termbox.SetCell(0, len(maze.grid)+2, char, termbox.ColorDefault, termbox.ColorDefault)
-		}
-		termbox.Flush()
-		return true
-	}
-
-	return false
-}
-
 // function for check if up, down, left or right is a wall
-func checkWall(maze *Maze, pos *map[string]int) []Direction {
-	list := []Direction{}
-
-	if maze.grid[(*pos)["x"]][(*pos)["y"]+1] != 1 {
-		list = append(list, Down)
-	}
-	if maze.grid[(*pos)["x"]][(*pos)["y"]-1] != 1 {
-		list = append(list, Up)
-	}
-	if maze.grid[(*pos)["x"]+1][(*pos)["y"]] != 1 {
-		list = append(list, Right)
-	}
-	if maze.grid[(*pos)["x"]-1][(*pos)["y"]] != 1 {
-		list = append(list, Left)
-	}
-
-	return list
-}
 
 func main() {
 	if err := termbox.Init(); err != nil {
@@ -161,46 +298,66 @@ func main() {
 	}
 
 	defer termbox.Close()
-	pos := map[string]int{"x": 1, "y": 1}
 
-	maze, _ := NewMaze()
-	maze.Render(&pos)
+	maze, errMaze := NewMaze()
+
+	if errMaze != nil {
+		fmt.Println(errMaze)
+		return
+	}
+
+	e := entity{
+		x:        1,
+		y:        1,
+		memory:   []uuid.UUID{},
+		maze:     maze,
+		acc:      10,
+		lastMove: Up,
+	}
+
+	check := e.checkWall(maze)
+	e.lastMove = check[rand.Intn(len(check))]
+
+	e.Render()
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
+	ticker := time.NewTicker(time.Second / time.Duration(e.acc))
 
 	go func() {
 		defer wg.Done()
 
-		ticker := time.NewTicker(time.Second / 20)
 		defer ticker.Stop()
 
 		for {
 			select {
 			case <-ticker.C:
-				if isWin(maze, &pos) {
+				if e.isWin() {
 					break
 				}
-				check := checkWall(maze, &pos)
+				check := e.checkWall(maze)
 				// fmt.Println(check)
 				selCheck := check[rand.Intn(len(check))]
 				// Mover aleatoriamente en una de las cuatro direcciones
 
 				switch selCheck {
 				case 0:
-					moveLeft(maze, &pos)
+					e.moveLeft()
+					e.Render()
 				case 1:
-					moveRight(maze, &pos)
+					e.moveRight()
+					e.Render()
 				case 2:
-					moveUp(maze, &pos)
+					e.moveUp()
+					e.Render()
 				case 3:
-					moveDown(maze, &pos)
+					e.moveDown()
+					e.Render()
 				}
 
-				maze.Render(&pos)
 			}
 		}
 	}()
@@ -210,22 +367,40 @@ func main() {
 
 		for {
 			ev := termbox.PollEvent()
+
 			switch ev.Type {
 			case termbox.EventKey:
+
 				switch ev.Key {
 				case termbox.KeyEsc:
 					return
+
 				case termbox.KeyArrowDown:
-					moveDown(maze, &pos)
+					e.moveDown()
+					e.Render()
 				case termbox.KeyArrowUp:
-					moveUp(maze, &pos)
+					e.moveUp()
+					e.Render()
 				case termbox.KeyArrowLeft:
-					moveLeft(maze, &pos)
+					e.moveLeft()
+					e.Render()
 				case termbox.KeyArrowRight:
-					moveRight(maze, &pos)
+					e.moveRight()
+					e.Render()
+				case termbox.KeyCtrlA:
+					if e.acc-1 > 0 {
+						e.acc--
+					}
+					ticker = time.NewTicker(time.Second / time.Duration(e.acc))
+				case termbox.KeyCtrlS:
+					e.acc++
+					ticker = time.NewTicker(time.Second / time.Duration(e.acc))
 				}
 
-				maze.Render(&pos)
+				if e.isWin() {
+					break
+				}
+
 			}
 		}
 	}()
